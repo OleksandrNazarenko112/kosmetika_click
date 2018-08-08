0abb2c45cdb3d03d6bfc06471de1f9b0daf895cc
AlexNaz <alex.n@ideasoft.io>
2018-08-08 13:03:21 +0300

    order

--- scripts/custom-scripts.js
+++ scripts/custom-scripts.js
@@ -38,8 +38,26 @@ function mobileFooter(){
     }
     mobileFooter();
 
-   
-
-
-
+    $('.basic-input').on('keyup', function(){
+        if($(this).val() != '') {
+        $('input[required]:focus + label').css('visibility', 'hidden');
+        }
+        if($(this).val() == '') {
+        $('input[required]:focus + label').css('visibility', 'visible');
+        }
+    });
+    $('#country-select li a').on('click', function(){
+        $('#country-btn').addClass('selected');
+        $('#country-btn').val($(this).text());
+        $('#country-btn').text($(this).text())
+    });
+    $('#shop-adress-select li a').on('click', function(){
+        $('#shop-adress-btn').addClass('selected');
+        $('#shop-adress-btn').val($(this).text());
+        $('#shop-adress-btn').text($(this).text())
+    });
+    $('.promo-code-container a').on('click', function(){
+        $(this).hide();
+        $('.promo-code-container input').addClass('show').focus();
+    })
 });
